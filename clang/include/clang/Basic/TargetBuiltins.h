23a4a306353f2ca0ed2fb69955059f0b17ec05fd
Laurent Thévenoux <lthevenoux@kalray.eu>
2019-08-01 15:06:56 +0200

    [k1c] Add basic builtin support
    Summary: This patch allows to build code containing Kalray's builtins.
    
    Test Plan: n/a
    
    Reviewers: O81 llvm-project!
    
    Differential Revision: https://phab.kalray.eu/D1545

--- clang/include/clang/Basic/TargetBuiltins.h
+++ clang/include/clang/Basic/TargetBuiltins.h
@@ -336,12 +336,23 @@ namespace clang {
     };
   }
 
+  /// K1C builtins
+  namespace K1C {
+    enum {
+      LastTIBuiltin = clang::Builtin::FirstTSBuiltin - 1,
+#define BUILTIN(ID, TYPE, ATTRS) BI##ID,
+#include "clang/Basic/BuiltinsK1C.def"
+      LastTSBuiltin
+    };
+  }
+
   static constexpr uint64_t LargestBuiltinID = std::max<uint64_t>(
       {ARM::LastTSBuiltin, AArch64::LastTSBuiltin, BPF::LastTSBuiltin,
        PPC::LastTSBuiltin, NVPTX::LastTSBuiltin, AMDGPU::LastTSBuiltin,
        X86::LastTSBuiltin, VE::LastTSBuiltin, RISCV::LastTSBuiltin,
        Hexagon::LastTSBuiltin, Mips::LastTSBuiltin, XCore::LastTSBuiltin,
-       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin});
+       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin, K1C::LastTSBuiltin});
+
 
 } // end namespace clang.
 
