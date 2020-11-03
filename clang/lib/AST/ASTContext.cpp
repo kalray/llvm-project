d0fbc378711530441fbeb26f73382905aa278c63
Diogo Sampaio <dsampaio@kalrayinc.com>
2020-11-03 10:37:00 +0100

    [kvx][tca] Add TCA builtins
    Summary:
    This patch extends Clang's builtin parser to be able to handle
    target specific types. Now, a type started with T passes the
    intrinsic types string to a parser to the TargetInfo, expecting
    the type and the string pointer in the correct position.
    
    This adds all requested TCA intrinsics, except lvc and swap. Swap
    will require some extra thinking in how to restrain the copy
    operations to be preserved inside a bundle, as the usual lowering
    happend before the packing of bundles.
    
    As for the builtins that perform inplace changes to a vectors, not
    returning a type seems the most logical manner, else it could give
    the user a false sensation that he could preserve the previous value.
    Perhaps, if required, that can be implemented, performing a vector
    copy before, but I think that was not the expeted behavior.
    The builtins that do not return are:
    	moveto[hi|lo]
    	convdhv[0-1]
    	convwbv[0-3]
    	fmma242hw[0-3]
    
    We also add operations that perform all the sub-parts operations,
    and returns a new TCA vector/wide type, them being:
    	moveoto
    	moveto
    	convdhv
    	convwbv
    	fmma242
    
    Test Plan: added tests
    
    Reviewers: lthevenoux, O81 llvm-coolidge
    
    Reviewed By: lthevenoux, O81 llvm-coolidge
    
    Differential Revision: https://phab.kalray.eu/D9300

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -10094,6 +10094,7 @@ static QualType DecodeTypeFromStr(const char *&Str, const ASTContext &Context,
   bool Signed = false, Unsigned = false;
   RequiresICE = false;
 
+  bool IsTargetType = false;
   // Read the prefixed modifiers first.
   bool Done = false;
   #ifndef NDEBUG
@@ -10102,6 +10103,9 @@ static QualType DecodeTypeFromStr(const char *&Str, const ASTContext &Context,
   while (!Done) {
     switch (*Str++) {
     default: Done = true; --Str; break;
+    case 'T':
+      IsTargetType = true;
+      break;
     case 'I':
       RequiresICE = true;
       break;
@@ -10185,6 +10189,13 @@ static QualType DecodeTypeFromStr(const char *&Str, const ASTContext &Context,
 
   QualType Type;
 
+  if (IsTargetType) {
+    bool Fail = Context.getTargetInfo().DecodeTargetTypeFromStr(
+        Str, Context, AllowTypeModifiers, Type);
+    if (Fail)
+      Error = ASTContext::GE_Missing_type;
+  } else {
+  // KVX: Do ne reindent this to avoid rebase issues.
   // Read the base type.
   switch (*Str++) {
   default: llvm_unreachable("Unknown builtin type letter!");
@@ -10369,6 +10380,7 @@ static QualType DecodeTypeFromStr(const char *&Str, const ASTContext &Context,
   case 'p':
     Type = Context.getProcessIDType();
     break;
+  }
   }
 
   // If there are modifiers and if we're allowed to parse them, go for it.
