bdaf0eaa3258186bcba33b9b7178391c4006ff2b
Xavier Raynaud <xavier.raynaud@kalray.eu>
2022-03-04 10:33:11 +0100

    [kvx] Reactivate tests that depend on kvx-cos and CodeGen/deactive thinlto_backend.ll
    Summary:
    ref T18088
    ref T18251
    
    Test Plan: CI
    
    Reviewers: O81 llvm-coolidge!
    
    Maniphest Tasks: T18251, T18088
    
    Differential Revision: https://phab.kalray.eu/D19020

--- clang/test/Driver/no-arc-exception-silence.m
+++ clang/test/Driver/no-arc-exception-silence.m
@@ -1,3 +1,2 @@
-// T18088
-// RUN: %clang -target x86_64 -Werror -fobjc-arc -fobjc-arc-exceptions -fno-objc-arc -Xclang -verify -c -o /dev/null %s
+// RUN: %clang -Werror -fobjc-arc -fobjc-arc-exceptions -fno-objc-arc -Xclang -verify -c -o /dev/null %s
 // expected-no-diagnostics
