c542b17009a631b022966f170d305af1771c52c2
Diogo Sampaio <dsampaio@kalrayinc.com>
2022-09-21 17:27:21 +0200

    [kvx][NFC] Rename `SCALARCOND` and add set CPU
    Summary:
    `SCALARCOND` and `LSUCOND` do have the same arguments and
    values, so the compiler can treat them equally. However, there are
    many more instructions using `LSUCOND` other than `SCALARCOND`
    (`CMOVED` only). So this renames the modifer to be consistent
    with most instructions.
    
    This also defines the default kvx `CPU` string to `kv3-1`, implements
    the target method to check if the current cpu string is equals to a
    given string and also implements a new target method to obtain the
    current cpu string.
    
    Test Plan: n/a
    
    Reviewers: csix, O81 llvm-coolidge
    
    Reviewed By: csix, O81 llvm-coolidge
    
    Maniphest Tasks: T18267
    
    Differential Revision: https://phab.kalray.eu/D23220

--- clang/lib/Basic/Targets/KVX.h
+++ clang/lib/Basic/Targets/KVX.h
@@ -68,6 +68,7 @@ public:
     resetDataLayout("e-S256-p:64:64-i1:8-i8:8-i16:16-i32:32-i64:64-"
                     "v64:64-v128:128-v256:256-v512:256-v1024:256-"
                     "f16:16-f32:32-f64:64-a:0:64-m:e-n32:64");
+    CPU = "kv3-1"; // Set the default cpu as kv3-1
   }
 
   void adjust(LangOptions &Opts) override;
@@ -98,6 +99,8 @@ public:
 
   bool isValidCPUName(StringRef Name) const override;
   bool setCPU(const std::string &Name) override;
+  bool validateCpuIs(StringRef Name) const override { return CPU == Name; };
+  StringRef getCPUstr() const override { return CPU; }
 
   void fillValidCPUList(SmallVectorImpl<StringRef> &Values) const override;
   bool DecodeTargetTypeFromStr(const char *&Str, const ASTContext &Context,
