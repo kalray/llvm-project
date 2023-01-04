2572a5a4347716cc631b5a892d0274d8e3845d85
Diogo Sampaio <dsampaio@kalrayinc.com>
2023-01-04 11:26:57 +0100

    [kvx][builtins] Miau miau, cat without super powers
    Summary:
    - Adds the `cat128/256/512` builtins, which concatenates equal typed
    vector of `64/128/256` bits into vector of `128/256/512` bits.
    
    Test Plan: added tests
    
    Reviewers: O81 llvm-coolidge!, csix
    
    Maniphest Tasks: T20349
    
    Differential Revision: https://phab.kalray.eu/D25911

--- llvm/include/llvm/IR/IRBuilder.h
+++ llvm/include/llvm/IR/IRBuilder.h
@@ -2314,6 +2314,20 @@ public:
     return CreateShuffleVector(V1, V2, IntMask, Name);
   }
 
+  Value *CreateVectorConcat(Value *V1, Value *V2, const Twine &Name = "") {
+    assert(V1->getType()->isVectorTy() && "Concatenating V1, not a vector.");
+    assert(V2->getType()->isVectorTy() && "Concatenating V2, not a vector.");
+    assert(V2->getType() == V1->getType() &&
+           "Concatenating distinct vector types.");
+    int Sz =
+        cast<VectorType>(V1->getType())->getElementCount().getFixedValue() * 2;
+    SmallVector<int, 16> Mask;
+    for (int I = 0; I < Sz; ++I)
+      Mask.push_back(I);
+
+    return CreateShuffleVector(V1, V2, Mask, Name);
+  }
+
   /// See class ShuffleVectorInst for a description of the mask representation.
   Value *CreateShuffleVector(Value *V1, Value *V2, ArrayRef<int> Mask,
                              const Twine &Name = "") {
