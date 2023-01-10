e36c4fbdd9fb578af96cb165a437fb118e022908
Diogo Sampaio <dsampaio@kalrayinc.com>
2023-01-10 09:37:50 +0100

    [kvx][NFC] Remove dead code
    Summary: Build vector for a single register types is done in patterns.
    
    Test Plan: n/a
    
    Reviewers: csix, O81 llvm-coolidge!
    
    Differential Revision: https://phab.kalray.eu/D26037

--- llvm/lib/Target/KVX/KVXISelLowering.h
+++ llvm/lib/Target/KVX/KVXISelLowering.h
@@ -142,8 +142,7 @@ private:
                                  bool Signed) const;
   SDValue lowerBlockAddress(SDValue Op, SelectionDAG &DAG) const;
   SDValue lowerBUILD_VECTOR(SDValue Op, SelectionDAG &DAG) const;
-  SDValue lowerBUILD_VECTOR_V2_64bit(SDValue Op, SelectionDAG &DAG,
-                                     bool useINSF = true) const;
+
   SDValue lowerBUILD_VECTOR_128bit(SDValue Op, SelectionDAG &DAG) const;
   SDValue lowerINSERT_VECTOR_ELT(SDValue Op, SelectionDAG &DAG) const;
   SDValue lowerINSERT_VECTOR_ELT_V4_128bit(SDLoc &DL, SelectionDAG &DAG,
