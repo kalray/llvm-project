ea56c034d7dc3fa7ac25573d855d890dcd9b6bfc
Cyril Six <cyril.six@kalray.eu>
2023-05-02 11:12:30 +0200

    [kvx] Add fmulho/fmulhx CV1 emulation
    Summary: Add fmulho and fmulhx macros to emulate their builtin in CV1
    
    Test Plan: new CodeGen test, similar patch D28644 passed libtensors unit tests
    
    Reviewers: dsampaio, O81 llvm-coolidge
    
    Reviewed By: dsampaio, O81 llvm-coolidge
    
    Maniphest Tasks: T21231
    
    Differential Revision: https://phab.kalray.eu/D28802

--- clang/lib/Headers/kvx_builtins.h
+++ clang/lib/Headers/kvx_builtins.h
@@ -1,4 +1,29 @@
 #ifndef __H__KVX_BUILTINS__
 #define __H__KVX_BUILTINS__
 
+#if defined(__llvm__) && defined(__kv3_1__)
+/** LLVM KV3-1 emulation of KV3-2 builtins */
+
+#define __get_v4i16_low(a) __builtin_shufflevector((a), (a), 0, 1, 2, 3)
+#define __get_v4i16_high(a) __builtin_shufflevector((a), (a), 4, 5, 6, 7)
+
+#define __builtin_kvx_fmulho(a, b, mod)                                        \
+  (__kvx_v8hi) __builtin_shufflevector(                                        \
+      __builtin_kvx_fmulhq(__get_v4i16_low(a), __get_v4i16_low(b), mod),       \
+      __builtin_kvx_fmulhq(__get_v4i16_high(a), __get_v4i16_high(b), mod), 0,  \
+      1, 2, 3, 4, 5, 6, 7)
+
+#define __get_v8i16_low(a)                                                     \
+  __builtin_shufflevector((a), (a), 0, 1, 2, 3, 4, 5, 6, 7)
+#define __get_v8i16_high(a)                                                    \
+  __builtin_shufflevector((a), (a), 8, 9, 10, 11, 12, 13, 14, 15)
+
+#define __builtin_kvx_fmulhx(a, b, mod)                                        \
+  (__kvx_v16hi) __builtin_shufflevector(                                       \
+      __builtin_kvx_fmulho(__get_v8i16_low(a), __get_v8i16_low(b), mod),       \
+      __builtin_kvx_fmulho(__get_v8i16_high(a), __get_v8i16_high(b), mod), 0,  \
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+
+#endif // defined(__llvm__) && defined(__kv3_1__)
+
 #endif /* __H__KVX_BUILTINS__ */
