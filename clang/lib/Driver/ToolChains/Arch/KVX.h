25e6c929f10a909c1ab58d26775f8f6e63123cdd
Diogo Sampaio <dsampaio@kalrayinc.com>
2024-06-05 16:40:51 +0200

    [kvx][mbr] Mimic gcc's linker call includes
    Summary: This mimics kvx-mbr-gcc linker call in regard of link folder,
    	files and libraries. It also adds the target option `-mhal`,
    	also to mimic gcc's behavior.
    	It also tells the clang to show kvx options for the driver, CC1
    	and opencl calls.
    
    Test Plan: changed files
    
    Reviewers: piannetta, csix, O81 llvm-coolidge!
    
    Differential Revision: https://phab.kalray.eu/D38568

---
+++ clang/lib/Driver/ToolChains/Arch/KVX.h
@@ -0,0 +1,23 @@
+//===--- KVX.h - KVX -                 specific Tool Helpers ----*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_ARCH_KVX_H
+#define LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_ARCH_KVX_H
+
+namespace clang {
+namespace driver {
+namespace tools {
+namespace KVX {
+
+constexpr char GCC_VER[] = "13.2.1";
+} // end namespace KVX
+} // end namespace tools
+} // end namespace driver
+} // end namespace clang
+
+#endif // LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_ARCH_KVX_H
--- clang/test/Driver/KVX/tools.c
+++ clang/test/Driver/KVX/tools.c
@@ -1,40 +1,40 @@
-// RUN: %clang --target=kvx-kalray-osp -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,OSP,KV3_1
-// RUN: %clang --target=kvx-kalray-osp -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,OSP,KV3_2
-// RUN: %clang --target=kvx-kalray-osp -v -o /dev/null %s -march=kv4-1 2>&1 | FileCheck %s --check-prefixes=ALL,OSP,KV4_1
-// RUN: %clang --target=kvx-kalray-cos -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_1
-// RUN: %clang --target=kvx-kalray-cos -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_2
+// RUN: %clang -### --target=kvx-kalray-mbr -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,MBR,KV3_1
+// RUN: %clang -### --target=kvx-kalray-mbr -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,MBR,KV3_2
+// RUN: %clang -### --target=kvx-kalray-mbr -v -o /dev/null %s -march=kv4-1 2>&1 | FileCheck %s --check-prefixes=ALL,MBR,KV4_1
+// RUN: %clang -### --target=kvx-kalray-cos -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_1
+// RUN: %clang -### --target=kvx-kalray-cos -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_2
 
-// RUN: %clang --target=kvx-osp -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,OSP,KV3_1
-// RUN: %clang --target=kvx-osp -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,OSP,KV3_2
-// RUN: %clang --target=kvx-osp -v -o /dev/null %s -march=kv4-1 2>&1 | FileCheck %s --check-prefixes=ALL,OSP,KV4_1
-// RUN: %clang --target=kvx-cos -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_1
-// RUN: %clang --target=kvx-cos -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_2
+// RUN: %clang -### --target=kvx-mbr -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,MBR,KV3_1
+// RUN: %clang -### --target=kvx-mbr -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,MBR,KV3_2
+// RUN: %clang -### --target=kvx-mbr -v -o /dev/null %s -march=kv4-1 2>&1 | FileCheck %s --check-prefixes=ALL,MBR,KV4_1
+// RUN: %clang -### --target=kvx-cos -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_1
+// RUN: %clang -### --target=kvx-cos -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_2
 
 // Downstream tests only, for default target = kvx
-// RUN: %clang --target=osp -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,OSP,KV3_1
-// RUN: %clang --target=osp -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,OSP,KV3_2
-// RUN: %clang --target=osp -v -o /dev/null %s -march=kv4-1 2>&1 | FileCheck %s --check-prefixes=ALL,OSP,KV4_1
-// RUN: %clang --target=cos -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_1
-// RUN: %clang --target=cos -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_2
+// RUN: %clang -### --target=mbr -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,MBR,KV3_1
+// RUN: %clang -### --target=mbr -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,MBR,KV3_2
+// RUN: %clang -### --target=mbr -v -o /dev/null %s -march=kv4-1 2>&1 | FileCheck %s --check-prefixes=ALL,MBR,KV4_1
+// RUN: %clang -### --target=cos -v -o /dev/null %s -march=kv3-1 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_1
+// RUN: %clang -### --target=cos -v -o /dev/null %s -march=kv3-2 2>&1 | FileCheck %s --check-prefixes=ALL,COS,KV3_2
 
 int main (void) { return 0; }
 
 // clang -cc1 command:
 
 // ALL-DAG: -no-integrated-as
-// OSP-DAG: -triple kvx-{{.*}}-osp
-// COS-DAG: -triple kvx-{{.*}}-cos
-// KV3_1-DAG: -target-cpu kv3-1
-// KV3_2-DAG: -target-cpu kv3-2
-// KV4_1-DAG: -target-cpu kv4-1
+// MBR-DAG: "-triple" "kvx-{{.*}}-mbr"
+// COS-DAG: "-triple" "kvx-{{.*}}-cos"
+// KV3_1-DAG: "-target-cpu" "kv3-1"
+// KV3_2-DAG: "-target-cpu" "kv3-2"
+// KV4_1-DAG: "-target-cpu" "kv4-1"
 
 // as command:
-// OSP: kvx-elf-as
+// MBR: kvx-mbr-as
 // COS: kvx-cos-as
 // KV3_1-DAG: march=kv3-1
 // KV3_2-DAG: march=kv3-2
 // KV4_1-DAG: march=kv4-1
 
 // ld command:
-// OSP: kvx-elf-ld
+// MBR: kvx-mbr-ld
 // COS: kvx-cos-ld
